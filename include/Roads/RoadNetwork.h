01b718f00d870f51068ae1886a9a3f2a751e5743
Pálinkás Lőrinc <palinkas.lorinc@gmail.com>
2025-06-10 00:18:13 +0200

    Implemented road network storage

--- include/Roads/RoadNetwork.h
+++ include/Roads/RoadNetwork.h
@@ -1,15 +1,27 @@
 #pragma once
 
-#include <map>
+#include <unordered_map>
 #include <memory>
 
 #include "Intersection.h"
 
+class Intersection;
+class Road;
+class Street;
+
 class RoadNetwork
 {
 private:
+    std::unordered_map<std::string, std::shared_ptr<Intersection>> m_IntersectionMap;
+    std::unordered_map<std::string, std::shared_ptr<Street>> m_StreetMap;
+    std::unordered_map<std::string, std::shared_ptr<Road>> m_RoadMap;
+
 public:
-    void Add(std::string id, std::shared_ptr<Intersection> added);
-    void Add(std::string id, std::shared_ptr<Road> added);
-    void Add(std::string id, std::shared_ptr<Street> added);
+    void Add(std::shared_ptr<Intersection> added);
+    void Add(std::shared_ptr<Road> added);
+    void Add(std::shared_ptr<Street> added);
+
+    std::shared_ptr<Intersection> GetIntersection(const std::string& id) const;
+    std::shared_ptr<Street> GetStreet(const std::string& id) const;
+
 };
--- src/Roads/RoadNetwork.cpp
+++ src/Roads/RoadNetwork.cpp
@@ -1,13 +1,26 @@
 #include <Roads/RoadNetwork.h>
 
-void RoadNetwork::Add(std::string id, std::shared_ptr<Intersection> added)
+void RoadNetwork::Add(std::shared_ptr<Intersection> added)
 {
+    m_IntersectionMap[added->ID()] = added;
 }
 
-void RoadNetwork::Add(std::string id, std::shared_ptr<Road> added)
+void RoadNetwork::Add(std::shared_ptr<Road> added)
 {
+    m_RoadMap[added->ID()] = added;
 }
 
-void RoadNetwork::Add(std::string id, std::shared_ptr<Street> added)
+void RoadNetwork::Add(std::shared_ptr<Street> added)
 {
+    m_StreetMap[added->ID()] = added;
+}
+
+std::shared_ptr<Intersection> RoadNetwork::GetIntersection(const std::string& id) const
+{
+    return m_IntersectionMap.at(id);
+}
+
+std::shared_ptr<Street> RoadNetwork::GetStreet(const std::string& id) const
+{
+    return m_StreetMap.at(id);
 }
