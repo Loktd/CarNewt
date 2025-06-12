0c06f81984c9483eb1469be9631ec9f2c84b3743
Pálinkás Lőrinc <palinkas.lorinc@gmail.com>
2025-06-12 23:15:08 +0200

    Updated simulation scene, so it display the road network and has minimal zoom and pan function for the camera

--- include/Scene/SimulationScene.h
+++ include/Scene/SimulationScene.h
@@ -6,13 +6,20 @@
 #include <Scene/SceneBase.h>
 #include <Loader/NewtLoader.h>
 #include <Engine/WindowingEngine.h>
+#include <Graphics/Camera2D.h>
+#include <Graphics/Shader2D.h>
 
 class SimulationScene : public SceneBase {
 private:
     NewtLoader m_Loader;
+
     std::shared_ptr<RoadNetwork> m_RoadNetwork;
+    std::shared_ptr<Camera2D> m_Camera;
+    std::shared_ptr<Shader2D> m_Shader;
+    
 public:
     SimulationScene();
     virtual void InitializeScene() override;
     virtual void HandleKeyPressed(int key) override;
+    virtual void Render() override;
 };
--- src/Scene/SimulationScene.cpp
+++ src/Scene/SimulationScene.cpp
@@ -1,6 +1,11 @@
 #include <Scene/SimulationScene.h>
 
-SimulationScene::SimulationScene() : SceneBase(glm::vec4(0, 1, 0, 1)), m_Loader() {}
+SimulationScene::SimulationScene() : SceneBase(glm::vec4(0, 1, 0, 1)), m_Loader() {
+    m_Camera = std::make_shared<Camera2D>(glm::vec2(0, 0), glm::vec2(800, 600));
+    m_Shader = std::make_shared<Shader2D>();
+
+    m_Shader->Use();
+}
 
 void SimulationScene::InitializeScene() {
     m_Loader.AskForDirectory();
@@ -11,4 +16,33 @@ void SimulationScene::HandleKeyPressed(int key) {
     if (key == GLFW_KEY_ESCAPE) {
         WindowingEngine::GetInstance().Exit();
     }
+
+    if (key == GLFW_KEY_E) {
+        m_Camera->Zoom(0.9);
+    }
+    if (key == GLFW_KEY_Q) {
+        m_Camera->Zoom(1.1);
+    }
+
+    if (key == GLFW_KEY_A) {
+        m_Camera->Pan(glm::vec2(-10, 0));
+    }
+    if (key == GLFW_KEY_D) {
+        m_Camera->Pan(glm::vec2(10, 0));
+    }
+    if (key == GLFW_KEY_W) {
+        m_Camera->Pan(glm::vec2(0, 10));
+    }
+    if (key == GLFW_KEY_S) {
+        m_Camera->Pan(glm::vec2(0, -10));
+    }
+
+    WindowingEngine::GetInstance().Refresh();
+}
+
+void SimulationScene::Render()
+{
+    SceneBase::Render();
+
+    m_RoadNetwork->Draw(m_Shader, m_Camera);
 }
